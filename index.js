c38a73aeb6b2a25c3d140aa0e70c67762d47a338
MH-liton-360 <liton72524nk@gmail.com>
2025-05-01 00:47:36 +0600

    vercel setup

--- index.js
+++ index.js
@@ -12,13 +12,11 @@ app.use(cors());
 app.use(express.json());
 
 
-//DB_USER = ctg_marketing
-//DB_PASS = MGdy77Cg1SxaTFiP
 
 
 
 
-const uri = `mongodb+srv://<db_username>:<db_password>@cluster0.uru7rsz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uru7rsz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
