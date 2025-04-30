1f6af76992da4ecaf060e3bc6aa35cfb82060027
MH-liton-360 <liton72524nk@gmail.com>
2025-05-01 00:29:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+//DB_USER = ctg_marketing
+//DB_PASS = MGdy77Cg1SxaTFiP
+
+
+
+
+const uri = `mongodb+srv://<db_username>:<db_password>@cluster0.uru7rsz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Car-cleaning success')
+})
+
+app.listen(port, () => {
+    console.log(`Car is waiting at: ${port}`)
+})
