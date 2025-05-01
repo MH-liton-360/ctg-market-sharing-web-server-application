37d54b27b51cb3df07719f3a6e181e7b250f17e7
MH-liton-360 <liton72524nk@gmail.com>
2025-05-01 18:01:39 +0600

    backend add card

--- index.js
+++ index.js
@@ -56,7 +56,6 @@ async function run() {
             res.send(result);
         });
 
-        console.log("Connected to MongoDB successfully");
     } catch (err) {
         console.error("MongoDB connection error:", err);
     }
